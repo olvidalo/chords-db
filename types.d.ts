0e78e8c5875ff91338964815f1ba41b973cec435
Marcel Schaeben <m.schaeben@uni-koeln.de>
2024-03-22 14:53:39 +0100

    add exports and typings

---
+++ types.d.ts
@@ -0,0 +1,53 @@
+export interface InstrumentChords {
+    main: Main;
+    tunings: Tunings;
+    keys: string[];
+    suffixes: string[];
+    chords: Chords;
+}
+
+
+export interface Chords {
+    [key: string]: ChordDef[];
+}
+
+export interface ChordDef {
+    key: string;
+    suffix: string;
+    positions: Position[];
+}
+
+
+export interface Position {
+    frets: number[];
+    fingers: number[];
+    baseFret: number;
+    barres: number[];
+    midi: number[];
+    capo?: boolean;
+}
+
+export interface Main {
+    strings: number;
+    fretsOnChord: number;
+    name: string;
+    numberOfChords: number;
+}
+
+export interface Tunings {
+    standard: string[];
+    [name: string]: string[];
+}
+
+export const guitar: InstrumentChords;
+export const ukulele: InstrumentChords;
+export const mandolin: InstrumentChords;
+
+export interface IChordsDB {
+    guitar: InstrumentChords;
+    ukulele: InstrumentChords;
+    mandolin: InstrumentChords;
+}
+
+declare const ChordsDB: IChordsDB;
+export default ChordsDB;
