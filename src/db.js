59ddc92adcacac2217cc1179f738d0541026c985
Marcel Schaeben <m.schaeben@uni-koeln.de>
2025-02-15 23:11:24 +0100

    Some renaming for consistency

--- src/db.js
+++ src/db.js
@@ -1,11 +1,11 @@
 import { default as guitar } from './db/guitar';
 import { default as ukulele } from './db/ukulele';
 import { default as mandolin } from './db/mandolin';
-import { default as ukulele_baritone } from './db/ukulele-baritone';
+import { default as ukuleleBaritone } from './db/ukulele-baritone';
 
 export default {
   guitar,
   ukulele,
   mandolin,
-  ukulele_baritone
+  "ukulele-baritone": ukuleleBaritone
 };
